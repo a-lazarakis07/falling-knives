c9278e77c7a2eb51a1fc82aacb341d2e7faf1007
a-lazarakis07 <a.lazarakis@mail.utoronto.ca>
2026-03-25 19:27:13 -0400

    Delete import.py

--- import.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("sp500.csv")   # <-- replace filename
-tickers = df.iloc[:, 0].dropna().astype(str).str.upper()
-
-tickers.to_csv("data/sp500_tickers.txt", index=False, header=False)
-print("Saved to data/sp500_tickers.txt")
